f64c333725add234d40fbb083cbcbe850ca90c82
Deepanshu <deepanshusingh8@deloitte.com>
2023-05-22 00:49:13 +0530

    String Swap of mini Assignment 3 is added

--- miniAssignment3/src/swapString/stringSwap.java
+++ miniAssignment3/src/swapString/stringSwap.java
@@ -6,30 +6,25 @@ import java.util.logging.Logger;
 public class stringSwap {
     private static final Logger logger = Logger.getLogger(stringSwap.class.getName());
 
-    public static void swapStrings(String firstString, String secondString) {
+    public static void swapStrings(StringBuilder str1, StringBuilder str2) {
         logger.log(Level.INFO, "Before swap:");
-        logger.log(Level.INFO, "firstString: " + firstString);
-        logger.log(Level.INFO, "secondString: " + secondString);
+        logger.log(Level.INFO, "First String: " + str1);
+        logger.log(Level.INFO, "Second String: " + str2);
 
-        // Append the second string to the first string
-        firstString = firstString + secondString;
-
-        // Extract the original second string from the updated first string
-        secondString = firstString.substring(0, firstString.length() - secondString.length());
-
-        // Extract the original first string from the updated first string
-        firstString = firstString.substring(secondString.length());
+        str1.append(str2);
+        str2 = new StringBuilder(str1.substring(0, str1.length() - str2.length()));
+        str1 = new StringBuilder(str1.substring(str2.length()));
 
         logger.log(Level.INFO, "After swap:");
-        logger.log(Level.INFO, "firstString: " + firstString);
-        logger.log(Level.INFO, "secondString: " + secondString);
+        logger.log(Level.INFO, "First String: " + str1);
+        logger.log(Level.INFO, "Second String: " + str2);
     }
 
     public static void main(String[] args) {
-        Logger.getLogger("").setLevel(Level.OFF); // Turn off logging from other loggers
+        Logger.getLogger("").setLevel(Level.INFO); // Set logger level to INFO
 
-        String firstString = "Hi Java";
-        String secondString = "Hello Python";
+        StringBuilder firstString = new StringBuilder("Hi Java");
+        StringBuilder secondString = new StringBuilder("Hello Python");
 
         swapStrings(firstString, secondString);
     }
